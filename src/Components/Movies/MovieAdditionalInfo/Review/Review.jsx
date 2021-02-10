7d663e8adaede06e665d81a87174fccf84cb01a9
Warness <65484013+Warness@users.noreply.github.com>
2021-02-10 23:55:10 +0200

    обзоры

--- src/Components/Movies/MovieAdditionalInfo/Review/Review.jsx
+++ src/Components/Movies/MovieAdditionalInfo/Review/Review.jsx
@@ -8,13 +8,22 @@ export default function Review({ dataObj }) {
     return <MoviePlaceholder />;
   }
 
+  function handleClick(event) {
+    console.log('showMore');
+  }
+
   return (
     <ul className={s.list}>
       {results.map(({ author, content, id }) => {
         return (
           <li key={id} className={s.item}>
             <h4 className={s.title}>{author}</h4>
-            <p className={s.text}>{content}</p>
+            <p className={s.text}>
+              {content}{' '}
+              <button className={s.button} type="button" onClick={handleClick}>
+                show more
+              </button>
+            </p>
           </li>
         );
       })}

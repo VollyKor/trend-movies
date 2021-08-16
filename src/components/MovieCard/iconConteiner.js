7792d3f5b3655955a1f1a1464d1213aab1c3aa10
VolKor <vol.kor.93.@gamil.com>
2021-08-16 20:38:35 +0300

    remove useless files

--- src/components/MovieCard/iconConteiner.js
+++
@@ -1,8 +0,0 @@
-import customIcons from './customIcons';
-
-function IconContainer(props) {
-  const { value, ...other } = props;
-  return <span {...other}>{customIcons[value].icon}</span>;
-}
-
-export default IconContainer;
